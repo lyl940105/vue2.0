76eda8f6fe3986a12219798cc88adccdfb22cee1
haha <850533584@qq.com>
2022-07-27 19:01:28 +0800

    增加bar组件

---
+++ src/components/Bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    我是一条bar
+  </div>
+</template>
+<script>
+export default {
+  name: 'Bar',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
     <h1>首页</h1>
+    <Bar></Bar>
   </div>
 </template>
 
 <script>
-
+import Bar from '@/components/Bar.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Bar
+  }
 }
 </script>
